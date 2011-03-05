af0cee4d898ffd6888976a8ff9bec322e6f0ada9
stevenhwu@gmail.com <stevenhwu@gmail.com@4e759364-6d8a-7a52-99c3-bc37ce0d8d84>
2011-03-05 23:56:20 +0000

    convert from googleDoc

---
+++ blue-beast/src/bb/mcmc/adapt/AdaptRatio.java
@@ -0,0 +1,23 @@
+package bb.mcmc.adapt;
+
+public class AdaptRatio {
+
+	public AdaptRatio(){
+		
+	}
+	
+	
+	public void calculateNewRatios(){
+		
+	}
+	
+	public void getRatios(){
+		
+	}
+	
+	
+	public void setRatios(){
+		
+	}
+	
+}
