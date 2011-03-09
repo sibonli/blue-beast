aec95eea3c7994ff9c3ae925c3b0933f7b275c8d
sibon.li@gmail.com <sibon.li@gmail.com@4e759364-6d8a-7a52-99c3-bc37ce0d8d84>
2011-03-09 01:17:05 +0000

    Changes to ProgressReports. Added methods to AdaptAcceptanceRatio

--- blue-beast/src/bb/mcmc/adapt/AdaptAcceptanceRatio.java
+++ blue-beast/src/bb/mcmc/adapt/AdaptAcceptanceRatio.java
@@ -5,10 +5,16 @@ import dr.inference.operators.MCMCOperator;
 public class AdaptAcceptanceRatio {
 
 	public AdaptAcceptanceRatio(MCMCOperator[] currentOperators){
-		
+		getRatios();
+        assessCurrentRatios();
+        calculateNewRatios();
+        setRatios();
 	}
 	
-	
+	public void assessCurrentRatios(){
+		// Code for this can probably be taken from BEAST (they give summary at end of run right?)
+	}
+
 	public void calculateNewRatios(){
 		
 	}
--- blue-beast/src/bb/report/ProgressReport.java
+++ blue-beast/src/bb/report/ProgressReport.java
@@ -2,18 +2,38 @@ package bb.report;
 
 public class ProgressReport {
 
-	public ProgressReport() {
-		
+    private double[] currentESSScores;
+    int essLowerLimitBoundary;
+
+	public ProgressReport(double[] currentESSScores, int essLowerLimitBoundary) {
+        this.currentESSScores = currentESSScores; /* These values should change according to changes in the original array */
+        this.essLowerLimitBoundary = essLowerLimitBoundary;
+		System.out.println("Note: Progress report is only a rough estimate");
 	}
 	
 	public double getSatas(){
 		return 0;
 		
 	}
+
+    public double calculateProgress(double[] currentESSScores, int ESSLowerLimitBoundary) {
+        double minESS = currentESSScores[0];
+        for(int i=1; i<currentESSScores.length; i++) {
+            if(currentESSScores[i]<minESS) {
+                minESS = currentESSScores[i];
+            }
+        }
+        return minESS/ESSLowerLimitBoundary;
+    }
 	
 	public void reportProgress(){
+        printProgress(calculateProgress(currentESSScores, essLowerLimitBoundary));
 		
-		
+	}
+    public void printProgress(double p){
+        int percentage = Math.round(((float) p));
+		System.out.println(percentage + "% complete");
+
 	}
 
 	
