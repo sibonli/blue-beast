0c3ff1254bae60ad7edb6a651022853cc4fbbb4e
sibon.li@gmail.com <sibon.li@gmail.com@4e759364-6d8a-7a52-99c3-bc37ce0d8d84>
2011-03-10 18:29:41 +0000

    Partitioned part of the functionality of BlueBeastMain.java into BlueBeast.java. Preliminary code added to chain length modifying classes

--- trunk/blue-beast/src/bb/mcmc/adapt/AdaptChainLengthInterval.java
+++ trunk/blue-beast/src/bb/mcmc/adapt/AdaptChainLengthInterval.java
@@ -3,11 +3,17 @@
 package bb.mcmc.adapt;
 
 
+import dr.inference.mcmc.MCMCOptions;
+
 /*
- * Adapt the chain length and the next interval we are checking
- */
+* Adapt the chain length and the next interval we are checking
+*/
 public class AdaptChainLengthInterval {
 
+    public void calculateNextCheckingInterval(MCMCOptions mcmcOptions){
+        //AdaptChainLengthInterval();
+        mcmcOptions.setChainLength(mcmcOptions.getChainLength() + 1000); // temp
+	}
 
 
 }
