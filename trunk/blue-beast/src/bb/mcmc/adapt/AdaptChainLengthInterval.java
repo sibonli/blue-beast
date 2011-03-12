eeeda2415bae889c5402fe79dc4772f6da379589
sibon.li@gmail.com <sibon.li@gmail.com@4e759364-6d8a-7a52-99c3-bc37ce0d8d84>
2011-03-12 10:22:46 +0000

    Added skeleton and TODO to AdaptChainLengthInterval

--- trunk/blue-beast/src/bb/mcmc/adapt/AdaptChainLengthInterval.java
+++ trunk/blue-beast/src/bb/mcmc/adapt/AdaptChainLengthInterval.java
@@ -3,17 +3,37 @@
 package bb.mcmc.adapt;
 
 
+import bb.mcmc.analysis.ConvergenceStatistic;
 import dr.inference.mcmc.MCMCOptions;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
 
 /*
 * Adapt the chain length and the next interval we are checking
 */
 public class AdaptChainLengthInterval {
 
-    public void calculateNextCheckingInterval(MCMCOptions mcmcOptions){
+    public static int calculateNextCheckingInterval(MCMCOptions mcmcOptions, Hashtable<String, ArrayList> traceInfo, ArrayList<ConvergenceStatistic> convergenceStatsToUse, boolean dynamicCheckingInterval){
         //AdaptChainLengthInterval();
-        mcmcOptions.setChainLength(mcmcOptions.getChainLength() + 1000); // temp
+        // TODO assess whether convergence has occurred based on the values of the summary statistics
+        boolean converged = assessConvergence(convergenceStatsToUse);
+        mcmcOptions.setChainLength(mcmcOptions.getChainLength() + 20); // temp
+
+        if(!converged) {
+            //TODO determine how long we need to continue running for, temporarily a constant
+            return mcmcOptions.getChainLength() + 10; //temp
+        }
+            /* Otherwise, if the chain is converged then stop the MCMC chain */
+            System.out.println("Statisics indicate that the variables are converged");
+            System.out.println("Stopping BEAST analysis");
+            mcmcOptions.setChainLength(mcmcOptions.getChainLength() + 1);
+            return mcmcOptions.getChainLength() + 1; //temp
 	}
+
+    public static boolean assessConvergence(ArrayList<ConvergenceStatistic> convergenceStatsToUse) {
+        return false;
+    }
 
 
 }
