fe675516436ba9727c85ae998a2470643d00b16c
sibon.li@gmail.com <sibon.li@gmail.com@4e759364-6d8a-7a52-99c3-bc37ce0d8d84>
2011-04-11 19:30:26 +0000

    A number of code changes. This version does not compile!

---
+++ blue-beast/src/beast2/core/BBMCMC.java
@@ -0,0 +1,42 @@
+package beast2.core;
+
+import dr.inference.mcmc.MCMC;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sibon
+ * Date: 3/15/11
+ * Time: 2:43 PM
+ * To change this template use File | Settings | File Templates.
+ *
+ * @author Wai Lok Sibon Li
+ *
+ */
+
+//@Description("Sibon's MCMC extension with periodical tests")
+
+public class BBMCMC extends MCMC {
+    public Input<Integer> m_period = new Input<Integer>("period","period between samples being tested (default 1000)", 1000);
+
+        public Input<Boolean> m_doTest = new Input<Boolean>("useTest","flag to indicate whether samples should be tested (default true)", true);
+
+        boolean m_bTest;
+        int m_nPeriod;
+
+    public BBMCMC(String s) {
+        super(s);
+    }
+
+    //@Override
+    public void initAndValidate() {
+            m_bTest = useTest.get();
+            m_nPeriod = m_period.get();
+        }
+
+    //@Override
+        void callUserFunction(int iSample) {
+            if (m_bTest && iSample % m_nPeriod==0) {
+                // do the work here
+            }
+        }
+}
