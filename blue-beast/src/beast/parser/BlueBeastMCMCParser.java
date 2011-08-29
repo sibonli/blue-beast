d9e48d951c6ad1074802f948535cdab3d60e5b3b
sibon.li@gmail.com <sibon.li@gmail.com@4e759364-6d8a-7a52-99c3-bc37ce0d8d84>
2011-08-29 23:18:31 +0000

    BlueBeastsMCMC is almost complete

--- blue-beast/src/beast/parser/BlueBeastMCMCParser.java
+++ blue-beast/src/beast/parser/BlueBeastMCMCParser.java
@@ -18,7 +18,14 @@
 
 package beast.parser;
 
+import bb.main.BlueBeast;
+import bb.mcmc.analysis.ConvergenceStatistic;
+import bb.mcmc.analysis.ESSConvergenceStatistic;
+import bb.mcmc.analysis.InterIntraChainVarianceConvergenceStatistic;
+import bb.mcmc.analysis.ZTempNovelConvergenceStatistic;
+import beast.core.BlueBeastMCMC;
 import dr.inference.loggers.Logger;
+import dr.inference.loggers.MCLogger;
 import dr.inference.markovchain.MarkovChain;
 import dr.inference.mcmc.MCMC;
 import dr.inference.mcmc.MCMCOptions;
@@ -29,7 +36,7 @@ import dr.xml.*;
 
 import java.util.ArrayList;
 
-public class BeastXMLParser extends AbstractXMLObjectParser {
+public class BlueBeastMCMCParser extends AbstractXMLObjectParser {
 
     public String getParserName() {
         return MCMC;
@@ -40,33 +47,14 @@ public class BeastXMLParser extends AbstractXMLObjectParser {
      */
     public Object parseXMLObject(XMLObject xo) throws XMLParseException {
 
-        MCMC mcmc = new MCMC(xo.getAttribute(NAME, "mcmc1"));
+        BlueBeastMCMC mcmc = new BlueBeastMCMC(xo.getAttribute(NAME, "mcmc1"));
         MCMCOptions options = new MCMCOptions();
         OperatorSchedule opsched = (OperatorSchedule) xo.getChild(OperatorSchedule.class);
         Likelihood likelihood = (Likelihood) xo.getChild(Likelihood.class);
+//        BlueBeast bb = (BlueBeast) xo.getChild(BlueBeast.class);
         ArrayList<Logger> loggers = new ArrayList<Logger>();
 
         likelihood.setUsed();
-
-        // check that all models, parameters and likelihoods are being used
-//        for (Likelihood l : Likelihood.FULL_LIKELIHOOD_SET) {
-//            if (!l.isUsed()) {
-//                java.util.logging.Logger.getLogger("dr.inference").warning("Likelihood, " + l.getId() +
-//                        ", of class " + l.getClass().getName() + " is not being handled by the MCMC.");
-//            }
-//        }
-//        for (Model m : Model.FULL_MODEL_SET) {
-//            if (!m.isUsed()) {
-//                java.util.logging.Logger.getLogger("dr.inference").warning("Model, " + m.getId() +
-//                        ", of class " + m.getClass().getName() + " is not being handled by the MCMC.");
-//            }
-//        }
-//        for (Parameter p : Parameter.FULL_PARAMETER_SET) {
-//            if (!p.isUsed()) {
-//                java.util.logging.Logger.getLogger("dr.inference").warning("Parameter, " + p.getId() +
-//                        ", of class " + p.getClass().getName() + " is not being handled by the MCMC.");
-//            }
-//        }
 
         options.setChainLength(xo.getIntegerAttribute(CHAIN_LENGTH));
         options.setUseCoercion(xo.getAttribute(COERCION, true));
@@ -78,10 +66,19 @@ public class BeastXMLParser extends AbstractXMLObjectParser {
         options.setFullEvaluationCount(xo.getAttribute(FULL_EVALUATION, 2000));
         options.setMinOperatorCountForFullEvaluation(xo.getAttribute(MIN_OPS_EVALUATIONS, 1));
 
+        if (xo.hasAttribute(CHECK_INTERVAL)) {
+            mcmc.setCheckInterval(xo.getIntegerAttribute(CHECK_INTERVAL));
+        }
+        else {
+            mcmc.setCheckInterval(1000);
+        }
+
+        ArrayList<String> varNames = new ArrayList<String>(xo.getChildCount());
         for (int i = 0; i < xo.getChildCount(); i++) {
             Object child = xo.getChild(i);
             if (child instanceof Logger) {
                 loggers.add((Logger) child);
+                varNames.add(((MCLogger) child).getColumn(0).getLabel()); // Hope this works
             }
         }
 
@@ -100,7 +97,33 @@ public class BeastXMLParser extends AbstractXMLObjectParser {
                 (options.fullEvaluationCount() == 0 ? "\n  full evaluation test off" : "")
                 );
 
-        mcmc.init(options, likelihood, opsched, loggerArray);
+
+        ArrayList<ConvergenceStatistic> convergenceStatsToUse = new ArrayList<ConvergenceStatistic>();
+        String convergenceStatsToUseParameters = xo.getAttribute(CONVERGENCE_STATS_TO_USE, "all");
+        if(convergenceStatsToUseParameters.equals("all")) {
+            convergenceStatsToUse.add(ESSConvergenceStatistic.INSTANCE);
+            convergenceStatsToUse.add(InterIntraChainVarianceConvergenceStatistic.INSTANCE);
+            convergenceStatsToUse.add(ZTempNovelConvergenceStatistic.INSTANCE);
+        }
+        else if(convergenceStatsToUseParameters.equals("ESS")) {
+            convergenceStatsToUse.add(ESSConvergenceStatistic.INSTANCE);
+        }
+        if(convergenceStatsToUseParameters.equals("interIntraChainVariance")) {
+            convergenceStatsToUse.add(InterIntraChainVarianceConvergenceStatistic.INSTANCE);
+        }
+
+        String[] variableNames = varNames.toArray(new String[varNames.size()]);
+
+        int essLowerLimitBoundary = xo.getAttribute(ESS_LOWER_LIMIT_BOUNDARY, 100);
+        double burninPercentage = xo.getAttribute(BURNIN_PERCENTAGE, 0.1);;
+        boolean dynamicCheckingInterval = xo.getAttribute(DYNAMIC_CHECKING_INTERVAL, true);
+        boolean autoOptimiseWeights = xo.getAttribute(AUTO_OPTIMISE_WEIGHTS, true);
+        boolean optimiseChainLength = xo.getAttribute(OPTIMISE_CHAIN_LENGTH, true);
+        int maxChainLength = xo.getAttribute(MAX_CHAIN_LENGTH, Integer.MAX_VALUE);
+        BlueBeast bb = new BlueBeast(opsched, options, convergenceStatsToUse, variableNames, essLowerLimitBoundary,
+                burninPercentage, dynamicCheckingInterval, autoOptimiseWeights, optimiseChainLength, maxChainLength);
+
+        mcmc.init(options, likelihood, opsched, loggerArray, bb);
 
         MarkovChain mc = mcmc.getMarkovChain();
         double initialScore = mc.getCurrentScore();
@@ -148,9 +171,21 @@ public class BeastXMLParser extends AbstractXMLObjectParser {
             AttributeRule.newBooleanRule(SPAWN, true),
             AttributeRule.newStringRule(NAME, true),
             AttributeRule.newStringRule(OPERATOR_ANALYSIS, true),
+            AttributeRule.newStringRule(CHECK_INTERVAL, true),
+
+            AttributeRule.newStringRule(CONVERGENCE_STATS_TO_USE, true),
+            AttributeRule.newIntegerRule(ESS_LOWER_LIMIT_BOUNDARY, true),
+            AttributeRule.newDoubleRule(BURNIN_PERCENTAGE, true),
+            AttributeRule.newBooleanRule(DYNAMIC_CHECKING_INTERVAL, true),
+            AttributeRule.newBooleanRule(AUTO_OPTIMISE_WEIGHTS, true),
+            AttributeRule.newBooleanRule(OPTIMISE_CHAIN_LENGTH, true),
+            AttributeRule.newIntegerRule(MAX_CHAIN_LENGTH, true),
+
             new ElementRule(OperatorSchedule.class),
             new ElementRule(Likelihood.class),
-            new ElementRule(Logger.class, 1, Integer.MAX_VALUE)
+            new ElementRule(Logger.class, 1, Integer.MAX_VALUE),
+
+//            new ElementRule(BlueBeast.class)
     };
 
     public static final String COERCION = "autoOptimize";
@@ -165,7 +200,14 @@ public class BeastXMLParser extends AbstractXMLObjectParser {
     public static final String TEMPERATURE = "temperature";
     public static final String SPAWN = "spawn";
     public static final String OPERATOR_ANALYSIS = "operatorAnalysis";
-
+    public static final String CHECK_INTERVAL = "checkInterval";
+    public static final String CONVERGENCE_STATS_TO_USE = "convergenceStatsToUse";
+    public static final String ESS_LOWER_LIMIT_BOUNDARY = "essLowerLimitBoundary";
+    public static final String BURNIN_PERCENTAGE = "burninPercentage";
+    public static final String DYNAMIC_CHECKING_INTERVAL = "dynamicCheckingInterval";
+    public static final String AUTO_OPTIMISE_WEIGHTS = "autoOptimiseWeights";
+    public static final String OPTIMISE_CHAIN_LENGTH = "optimiseChainLength";
+    public static final String MAX_CHAIN_LENGTH = "maxChainLength";
 
 
 
